997ab6d96c97568e2151dee70d8ecdd88880eb02
ajollymarlier <ajollymarlier@bayviewglen.ca>
2018-07-21 17:04:38 -0400

    Added Other practise files

---
+++ Objects.py
@@ -0,0 +1,69 @@
+import os
+
+class Car:
+    def __init__(self, speed, color):
+        self.speed = speed
+        self.color = color
+
+    def sayState(self):
+        print("My color is {} and I'm going {}km/h".format(self.color, self.speed))
+
+    def setSpeed(self, speed):
+        self.speed = speed
+
+    def setColor(self, color):
+        self.color = color;
+
+    def getSpeed(self):
+        return self.speed
+
+    def getColor(self):
+        return self.color
+
+    def accelerate(self):
+        self.speed += 2
+
+    def deccelerate(self):
+        self.speed -= 2;
+
+class Lambo(Car):
+    __rapper = ""
+
+    def __init__(self, speed, color, rapper):
+        self.__rapper = rapper
+        super(Car, self).__init__(speed, color)
+
+
+
+def doStuff():
+    myCar = Car(int(input("Enter a starting speed: ")), input("Enter a color: "))
+
+    myCar.deccelerate()
+    myCar.sayState()
+
+    for i in range(11):
+        myCar.accelerate()
+
+    myCar.sayState()
+
+    clearAnswer = input("Clear Console? (Y/N)")
+
+    if clearAnswer.upper() == "Y":
+        os.system('cls')
+
+    huracan = Lambo(4, "Purple", "KSI")
+
+    print(huracan.getColor())
+
+doStuff()
+
+
+
+
+
+
+
+
+
+
+
